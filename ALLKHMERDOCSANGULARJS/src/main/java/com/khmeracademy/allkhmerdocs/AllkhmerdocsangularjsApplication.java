ea841d1f101ce2cbf391af59825d4fe86fc4918c
mineachem <minea.chem@gmail.com>
2016-08-14 11:57:31 +0700

    add new project

---
+++ ALLKHMERDOCSANGULARJS/src/main/java/com/khmeracademy/allkhmerdocs/AllkhmerdocsangularjsApplication.java
@@ -0,0 +1,12 @@
+package com.khmeracademy.allkhmerdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllkhmerdocsangularjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllkhmerdocsangularjsApplication.class, args);
+	}
+}
---
+++ ALLKHMERDOCSANGULARJS/src/test/java/com/khmeracademy/allkhmerdocs/AllkhmerdocsangularjsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.khmeracademy.allkhmerdocs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AllkhmerdocsangularjsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
